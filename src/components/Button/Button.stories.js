72e7e0ea6c29986029b88374140a22aecbedcc4a
Morne Louw <morne.louw@saongroup.com>
2021-09-29 18:12:05 +0200

    Added some args to the Basic Story, and select-list controls also an onCLick event, to investigate how this is done is StoryBook

--- src/components/Button/Button.stories.js
+++ src/components/Button/Button.stories.js
@@ -4,12 +4,30 @@ import Button from './Button'
 
 export default {
     title: 'MUI/Button',
-    component: Button
+    component: Button,
+    argTypes: {
+        variant: {
+            options: ['text', 'contained', 'outlined'],
+            control: { type: 'select' }
+        },
+        color: {
+            options: ['info', 'primary', 'secondary', 'success', 'error', 'warning'],
+            control: { type: 'select' }
+        },
+        size: {
+            options: ['small', 'medium', 'large'],
+            control: { type: 'select' }
+        },
+        onClickHandler: { action: 'clicked' }
+    }
 }
 
 const Template = (args) => <Button {...args} />;
 
 export const Basic = Template.bind({});
 Basic.args = {
+    variant: 'contained',
+    color: 'primary',
+    size: 'medium',
     children: 'Button Label'
 }
