e2ab361bce27890bd30df165a77d9b39d293c79c
Morne Louw <morne.louw@saongroup.com>
2021-09-30 06:56:57 +0200

    Copied the Button story to the general stories folder

---
+++ src/stories/MUI/Button.stories.js
@@ -0,0 +1,59 @@
+import React from 'react';
+
+import Button from "@mui/material/Button"
+import DeleteIcon from '@mui/icons-material/Delete';
+import SendIcon from '@mui/icons-material/Send';
+
+export default {
+    title: 'Material-UI/Button',
+    component: Button,
+    argTypes: {
+        variant: {
+            options: ['text', 'contained', 'outlined'],
+            control: { type: 'select' }
+        },
+        color: {
+            options: ['info', 'primary', 'secondary', 'success', 'error', 'warning'],
+            control: { type: 'select' }
+        },
+        size: {
+            options: ['small', 'medium', 'large'],
+            control: { type: 'select' }
+        },
+        onClick: { action: 'clicked' }
+    }
+}
+
+const Template = (args) => <Button {...args} />;
+
+export const Basic = Template.bind({});
+Basic.args = {
+    variant: 'contained',
+    color: 'primary',
+    size: 'medium',
+    disabled: false,
+    disableElevation: false,
+    children: 'Button Label'
+}
+
+export const WithStartIcon = Template.bind({});
+WithStartIcon.args = {
+    ...Basic.args,
+    startIcon: <DeleteIcon />,
+    children: 'Delete'
+}
+
+export const WithEndIcon = Template.bind({});
+WithEndIcon.args = {
+    ...Basic.args,
+    endIcon: <SendIcon />,
+    children: 'Send'
+}
+
+export const WithBothIcons = Template.bind({});
+WithBothIcons.args = {
+    ...Basic.args,
+    startIcon: <DeleteIcon />,
+    endIcon: <SendIcon />,
+    children: 'Delete and Send'
+}
