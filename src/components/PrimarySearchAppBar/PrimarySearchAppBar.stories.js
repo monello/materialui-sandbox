af5ba15cc1ab20d142e2c9e2e27f8ffddfff18d9
Morne Louw <morne.louw@saongroup.com>
2021-10-01 06:05:42 +0200

    Added PrimarySearchAppBar component from MUI sandbox, and related Story

---
+++ src/components/PrimarySearchAppBar/PrimarySearchAppBar.stories.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import PrimarySearchAppBar from './PrimarySearchAppBar'
+
+export default {
+    title: 'Material-UI/PrimarySearchAppBar',
+    component: PrimarySearchAppBar
+}
+
+const Template = (args) => <PrimarySearchAppBar {...args} />;
+
+export const Basic = Template.bind({});
