fd3da438ecae502eb9ba1dd0f7d29a207c847bf7
Morne Louw <morne.louw@saongroup.com>
2021-10-02 07:03:02 +0200

    Added a MUI Dialog wrapper component and related Story

---
+++ src/components/Dialog/Dialog.stories.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import Dialog from './DIalog'
+
+export default {
+    title: 'Material-UI/Dialog',
+    component: Dialog
+}
+
+const Template = (args) => <Dialog {...args} />;
+
+export const Basic = Template.bind({});
