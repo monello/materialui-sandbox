176b693b6aa981900cf465390fecb7f2bc273ebe
Morne Louw <morne.louw@saongroup.com>
2021-10-02 15:24:27 +0200

    Moved the Dialog buttons to the Story because they don't actually make out part of the Dialog component

--- src/components/Dialog/DIalog.js
+++ src/components/Dialog/DIalog.js
@@ -24,19 +24,8 @@ function PaperComponent(props) {
     );
 }
 
-export default function Dialog() {
-    const [open, setOpen] = React.useState(false);
-    const [scroll, setScroll] = React.useState('paper');
-
-    const handleClickOpen = (scrollType) => () => {
-        setOpen(true);
-        setScroll(scrollType);
-    };
-
-    const handleClose = () => {
-        setOpen(false);
-    };
-
+export default function Dialog(props) {
+    const { open, scroll, handleClose } = props;
     const descriptionElementRef = React.useRef(null);
     React.useEffect(() => {
         if (open) {
@@ -48,40 +37,36 @@ export default function Dialog() {
     }, [open]);
 
     return (
-        <div>
-            <Button onClick={handleClickOpen('paper')}>scroll=paper</Button>
-            <Button onClick={handleClickOpen('body')}>scroll=body</Button>
-            <MuiDialog
-                open={open}
-                onClose={handleClose}
-                scroll={scroll}
-                aria-labelledby="scroll-dialog-title"
-                aria-describedby="scroll-dialog-description"
-                PaperComponent={PaperComponent}
-                TransitionComponent={Transition}
-            >
-                <DialogTitle style={{ cursor: 'move' }} id="scroll-dialog-title">Subscribe</DialogTitle>
-                <DialogContent dividers={scroll === 'paper'}>
-                    <DialogContentText
-                        id="scroll-dialog-description"
-                        ref={descriptionElementRef}
-                        tabIndex={-1}
-                    >
-                        {[...new Array(10)]
-                            .map(
-                                () => `Crasx mattis consectetur purus sit amet fermentum.
+        <MuiDialog
+            open={open}
+            onClose={handleClose}
+            scroll={scroll}
+            aria-labelledby="scroll-dialog-title"
+            aria-describedby="scroll-dialog-description"
+            PaperComponent={PaperComponent}
+            TransitionComponent={Transition}
+        >
+            <DialogTitle style={{ cursor: 'move' }} id="scroll-dialog-title">Subscribe</DialogTitle>
+            <DialogContent dividers={scroll === 'paper'}>
+                <DialogContentText
+                    id="scroll-dialog-description"
+                    ref={descriptionElementRef}
+                    tabIndex={-1}
+                >
+                    {[...new Array(10)]
+                        .map(
+                            () => `Crasx mattis consectetur purus sit amet fermentum.
 Cras justo odio, dapibus ac facilisis in, egestas eget quam.
 Morbi leo risus, porta ac consectetur ac, vestibulum at eros.
 Praesent commodo cursus magna, vel scelerisque nisl consectetur et.`,
-                            )
-                            .join('\n')}
-                    </DialogContentText>
-                </DialogContent>
-                <DialogActions>
-                    <Button onClick={handleClose}>Cancel</Button>
-                    <Button onClick={handleClose}>Subscribe</Button>
-                </DialogActions>
-            </MuiDialog>
-        </div>
+                        )
+                        .join('\n')}
+                </DialogContentText>
+            </DialogContent>
+            <DialogActions>
+                <Button onClick={handleClose}>Cancel</Button>
+                <Button onClick={handleClose}>Subscribe</Button>
+            </DialogActions>
+        </MuiDialog>
     );
 }
--- src/components/Dialog/Dialog.stories.js
+++ src/components/Dialog/Dialog.stories.js
@@ -1,12 +1,56 @@
 import React from 'react';
 
 import Dialog from './Dialog'
+import Button from '@mui/material/Button';
+
+import { addDecorator } from "@storybook/react";
 
 export default {
     title: 'Material-UI/Dialog',
-    component: Dialog
+    component: Dialog,
+    // decorators: [
+    //     (Story) => {
+    //         const [open, setOpen] = React.useState(false);
+    //         const [scroll, setScroll] = React.useState('paper');
+    //         const handleClickOpen = (scrollType) => () => {
+    //             setOpen(true);
+    //             setScroll(scrollType);
+    //         };
+    //         const handleClose = () => {
+    //             setOpen(false);
+    //         };
+    //         return (
+    //             <>
+    //                 <Story open={open} scroll={scroll} handleClose={handleClose} />
+    //                 <Button variant="text" onClick={handleClickOpen('paper')}>Open (Scrollabble, Paper)</Button>
+    //                 <br />
+    //                 <Button variant="text" onClick={handleClickOpen('body')}>Open (Scrollable Body)</Button>
+    //             </>
+    //         )
+    //     },
+    // ],
 }
 
-const Template = (args) => <Dialog {...args} />;
+// addDecorator(story => <div className="platform">{story()}</div>);
+
+const Template = (args) => {
+    const [open, setOpen] = React.useState(false);
+    const [scroll, setScroll] = React.useState('paper');
+    const handleClickOpen = (scrollType) => () => {
+        setOpen(true);
+        setScroll(scrollType);
+    };
+    const handleClose = () => {
+        setOpen(false);
+    };
+    return (
+        <>
+            <Dialog {...args} open={open} scroll={scroll} handleClose={handleClose} />
+            <Button variant="text" onClick={handleClickOpen('paper')}>Open (Scrollabble, Paper)</Button>
+            <br />
+            <Button variant="text" onClick={handleClickOpen('body')}>Open (Scrollable Body)</Button>
+        </>
+    )
+};
 
 export const Basic = Template.bind({});
