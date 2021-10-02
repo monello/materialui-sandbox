04651086db273665f669f2eedffb826f4f55bae2
Morne Louw <morne.louw@saongroup.com>
2021-10-02 08:40:48 +0200

    Added a SpeedDial component and related Story

---
+++ src/components/SpeedDial/SpeedDial.stories.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import SpeedDial from './SpeedDial'
+
+export default {
+    title: 'Material-UI/SpeedDial',
+    component: SpeedDial
+}
+
+const Template = (args) => <SpeedDial {...args} />;
+
+export const Basic = Template.bind({});
